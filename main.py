c75eaf25b3085b5b532be59bdbafd71e884241ff
GeorgieWasTaken <egeorgievskij@gmail.com>
2023-02-23 20:38:31 +0300

    добавлено уведомление о запуске бота Егору и Феде

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import handlers
 
 async def start_up(message=types.Message):
 
-   await bot.send_message(chat_id=str(339925580), text="Бот запущен и готов к работе")
-   await bot.send_message(chat_id=str(408576223), text="Бот запущен и готов к работе")
+   await bot.send_message(chat_id=str(339925580), text="Бот запущен и готов к работе!")
+   await bot.send_message(chat_id=str(408576223), text="Бот запущен и готов к работе!")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=start_up)
