d958b93f1e681f266b82daafef104caedcbbe5e9
GeorgieWasTaken <egeorgievskij@gmail.com>
2023-02-23 18:15:50 +0300

    добавлено уведомление о запуске бота админу

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import handlers
 
 async def start_up(message=types.Message):
 
-    """await bot.send_message(chat_id=admin_id, text="Бот запущен и готов к работе")"""
+   await bot.send_message(chat_id=admin_id, text="Бот запущен и готов к работе")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=start_up)
