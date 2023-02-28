c041e775dac34851db50bc643536edccb5f23262
iamboneidle <fedapanfilov@gmail.com>
2023-02-28 22:51:56 +0300

    убрана возможность админа присылать пользователю сообщения после того, как тот остановил бот

--- handlers/handler.py
+++ handlers/handler.py
@@ -9,22 +9,36 @@ from config import dp, bot, admin_id
 from keyboards import answer_on_menu, menu, stop_the_bot
 from aiogram.types import ReplyKeyboardRemove
 from config import dp
+
 """Фильтр на сообщения от админа"""
 from aiogram.dispatcher.filters import BoundFilter
+
+
 class MyFilter(BoundFilter):
     key = 'is_admin'
+
     def __init__(self, is_admin):
         self.is_admin = is_admin
+
     async def check(self, message: types.Message):
         member = await bot.get_chat_member(message.chat.id, message.from_user.id)
         return member.is_chat_admin()
+
+
 dp.filters_factory.bind(MyFilter)
+
 """Бот спрашивает, к какому типу относится вопрос"""
+
+
 @dp.message_handler(Text(equals=["Задать вопрос"]))
 async def conv_start(message: types.Message):
     await message.answer("Какой тип вопроса?", reply_markup=answer_on_menu)
     await Questions.topictheme.set()
+
+
 """Бот задает тему диалога"""
+
+
 @dp.message_handler(state=Questions.topictheme)
 async def conv_start(message: types.Message):
     global typeQ
@@ -34,11 +48,11 @@ async def conv_start(message: types.Message):
         typeQ = "Общий"
     await message.answer("Какова тема?", reply_markup=ReplyKeyboardRemove())
     await Questions.typeQ.set()
+
+
 @dp.message_handler(state=Questions.typeQ)
 async def conv_start(message: types.Message):
     theme = message.text
-    global is_active
-    is_active = True
 
     await message.answer("Создан чат с коучем. Задавайте вопрос", reply_markup=stop_the_bot)
     global user_id
@@ -49,29 +63,30 @@ async def conv_start(message: types.Message):
     await bot.send_message(chat_id='@helpbot_bot_bot_bot', message_thread_id=topic,
                            text="Сейчас будет задан анонимный вопрос")
     await Questions.start.set()
-"""Юзер задает вопрос коучу + остановка бота для юзера"""
+
+
+"""Юзер задает вопрос коучу"""
+
+
 @dp.message_handler(state=Questions.start)
 async def asking(message: types.Message, state: FSMContext):
     text = message.text
 
-    if text == "Остановить диалог":
+    if text == "Остановить бота":
         await message.answer('Чат удален', reply_markup=menu)
-
-        global is_active
-        is_active = False
         await state.finish()
         return
-    if is_active == True:
-        await bot.send_message(chat_id='@helpbot_bot_bot_bot', message_thread_id=topic, text=text)
-"""Сообщения от админа, которые бот берет из топика и отправляет юзеру + остановка бота для коуча"""
+
+    await bot.send_message(chat_id='@helpbot_bot_bot_bot', message_thread_id=topic, text=text)
+
+
+"""Сообщения от админа, которые бот берет из топика и отправляет юзеру"""
+
+
 @dp.message_handler(is_admin=True)
-async def answ(message: types.Message, state: FSMContext):
+async def answ(message: types.Message):
     text = message.text
-    if is_active == True:
-        await bot.send_message(chat_id=user_id, text=text)
+    await bot.send_message(chat_id=user_id, text=text)
 
-    else:
-        await bot.send_message(chat_id='@helpbot_bot_bot_bot', message_thread_id=topic, text='разговор закончен')
 
-        await state.finish()
-        return
+"""Остановка бота"""
