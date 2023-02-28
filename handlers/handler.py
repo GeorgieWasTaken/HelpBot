5a769ff791c77b36821b5b097193dbccc8828039
GeorgieWasTaken <egeorgievskij@gmail.com>
2023-03-01 01:15:45 +0300

    .

--- handlers/handler.py
+++ handlers/handler.py
@@ -1,4 +1,4 @@
-#мяу
+# мяу
 import aiogram
 import logging
 from aiogram import types
@@ -9,36 +9,22 @@ from config import dp, bot, admin_id
 from keyboards import answer_on_menu, menu, stop_the_bot
 from aiogram.types import ReplyKeyboardRemove
 from config import dp
-
 """Фильтр на сообщения от админа"""
 from aiogram.dispatcher.filters import BoundFilter
-
-
 class MyFilter(BoundFilter):
     key = 'is_admin'
-
     def __init__(self, is_admin):
         self.is_admin = is_admin
-
     async def check(self, message: types.Message):
         member = await bot.get_chat_member(message.chat.id, message.from_user.id)
         return member.is_chat_admin()
-
-
 dp.filters_factory.bind(MyFilter)
-
 """Бот спрашивает, к какому типу относится вопрос"""
-
-
 @dp.message_handler(Text(equals=["Задать вопрос"]))
 async def conv_start(message: types.Message):
     await message.answer("Какой тип вопроса?", reply_markup=answer_on_menu)
     await Questions.topictheme.set()
-
-
 """Бот задает тему диалога"""
-
-
 @dp.message_handler(state=Questions.topictheme)
 async def conv_start(message: types.Message):
     global typeQ
@@ -48,11 +34,11 @@ async def conv_start(message: types.Message):
         typeQ = "Общий"
     await message.answer("Какова тема?", reply_markup=ReplyKeyboardRemove())
     await Questions.typeQ.set()
-
-
 @dp.message_handler(state=Questions.typeQ)
 async def conv_start(message: types.Message):
     theme = message.text
+    global is_active
+    is_active = True
 
     await message.answer("Создан чат с коучем. Задавайте вопрос", reply_markup=stop_the_bot)
     global user_id
@@ -63,40 +49,29 @@ async def conv_start(message: types.Message):
     await bot.send_message(chat_id='@helpbot_bot_bot_bot', message_thread_id=topic,
                            text="Сейчас будет задан анонимный вопрос")
     await Questions.start.set()
-
-
-"""Юзер задает вопрос коучу"""
-
-
+"""Юзер задает вопрос коучу + остановка бота для юзера"""
 @dp.message_handler(state=Questions.start)
 async def asking(message: types.Message, state: FSMContext):
-     text = message.text
-
-     if text == "Остановить бота":
-         await message.answer('Разговор закончен', reply_markup=menu)
-         await state.finish()
-         return
-
-     await bot.send_message(chat_id='@helpbot_bot_bot_bot', message_thread_id=topic, text=text)
-
-
-"""Сообщения от админа, которые бот берет из топика и отправляет юзеру"""
-
-
-@dp.message_handler(is_admin=True)
-async def answ(message: types.Message):
     text = message.text
-    await bot.send_message(chat_id=user_id, text=text)
 
+    if text == "Остановить диалог":
+        await message.answer('Чат удален', reply_markup=menu)
 
-# """Остановка бота"""
-#
-#
-# @dp.message_handler(Text(equals=["Остановить бота"]))
-# async def stopping(message: types.Message):
-#     await message.answer('Чат удален', reply_markup=menu)
+        global is_active
+        is_active = False
+        await state.finish()
+        return
+    if is_active == True:
+        await bot.send_message(chat_id='@helpbot_bot_bot_bot', message_thread_id=topic, text=text)
+"""Сообщения от админа, которые бот берет из топика и отправляет юзеру + остановка бота для коуча"""
+@dp.message_handler(is_admin=True)
+async def answ(message: types.Message, state: FSMContext):
+    text = message.text
+    if is_active == True:
+        await bot.send_message(chat_id=user_id, text=text)
 
+    else:
+        await bot.send_message(chat_id='@helpbot_bot_bot_bot', message_thread_id=topic, text='разговор закончен')
 
-
-
-
+        await state.finish()
+        return
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import handlers
 
 async def start_up(message=types.Message):
     await bot.send_message(chat_id=str(339925580), text="Бот запущен и готов к работе!",reply_markup=menu)
-    await bot.send_message(chat_id=str(408576223), text="Бот запущен и готов к работе!", reply_markup=menu)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=start_up)
