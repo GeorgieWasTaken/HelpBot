07129dfa22b334ec8e012cf8a3236a057b1f3ed2
GeorgieWasTaken <egeorgievskij@gmail.com>
2023-02-28 17:05:11 +0300

    исправил кнопку остановить бота, добавил оповещение Феди о запуске бота, добавил появление клавиатуры menu при запуске бота

--- handlers/handler.py
+++ handlers/handler.py
@@ -1,4 +1,4 @@
-# мяу
+#мяу
 import aiogram
 import logging
 from aiogram import types
@@ -70,14 +70,14 @@ async def conv_start(message: types.Message):
 
 @dp.message_handler(state=Questions.start)
 async def asking(message: types.Message, state: FSMContext):
-    text = message.text
+     text = message.text
 
-    if text == "Остановить бота":
-        await message.answer('Чат удален', reply_markup=menu)
-        await state.finish()
-        return
+     if text == "Остановить бота":
+         await message.answer('Разговор закончен', reply_markup=menu)
+         await state.finish()
+         return
 
-    await bot.send_message(chat_id='@helpbot_bot_bot_bot', message_thread_id=topic, text=text)
+     await bot.send_message(chat_id='@helpbot_bot_bot_bot', message_thread_id=topic, text=text)
 
 
 """Сообщения от админа, которые бот берет из топика и отправляет юзеру"""
@@ -89,4 +89,14 @@ async def answ(message: types.Message):
     await bot.send_message(chat_id=user_id, text=text)
 
 
-"""Остановка бота"""
+# """Остановка бота"""
+#
+#
+# @dp.message_handler(Text(equals=["Остановить бота"]))
+# async def stopping(message: types.Message):
+#     await message.answer('Чат удален', reply_markup=menu)
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import asyncio
 import logging
 from config import dp, bot, admin_id
 from aiogram import types, executor
-
+from keyboards import menu
 import handlers
 
 
 async def start_up(message=types.Message):
-    await bot.send_message(chat_id=str(339925580), text="Бот запущен и готов к работе!")
-
+    await bot.send_message(chat_id=str(339925580), text="Бот запущен и готов к работе!",reply_markup=menu)
+    await bot.send_message(chat_id=str(408576223), text="Бот запущен и готов к работе!", reply_markup=menu)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=start_up)
